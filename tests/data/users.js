cc7b810de9ec038c464e920e515c7789086c9f85
Kirstie <Kirstie@Wildmans-MacBook-Air.local>
2022-09-22 10:06:35 +0100

    Removing test

--- tests/data/users.js
+++ tests/data/users.js
@@ -2,7 +2,6 @@ const users = {
   smoke: [
     { firstName: 'smoke-test', email: 'smoke-test@example.com', password: 'password123!', rememberMe: false },
     { firstName: 'new-user-test', email: '@smoketest.com', password: 'Password123', rememberMe: false },
-    { firstName: 'transaction-test', email: 'rapid999@me.com', password: 'Test1234', rememberMe: false }
   ],
   stress: [
     { firstName: 'test', email: 'test@sw0.com', password: 'Password123', rememberMe: false },
